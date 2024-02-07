4282459f58612036bcc13cf8544cf1fe35cb79ba
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-02-07 18:01:09 +0530

    remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./App.css"
 
 /**
  * ?  =====Import Components=====
  */
-import Home from './Pages/Home';
-import Signup from './Components/Signup/Signup';
-import Login from './Components/Login/Login';
-import Create from './Components/Create/Create';
-import ViewPost from './Pages/ViewPost';
+import Home from "./Pages/Home"
+import Signup from "./Components/Signup/Signup"
+import Login from "./Components/Login/Login"
+import Create from "./Components/Create/Create"
+import ViewPost from "./Pages/ViewPost"
 
 function App() {
   return (
@@ -17,15 +16,15 @@ function App() {
       <Router>
         <Routes>
           {/* Use Route components directly */}
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/view' element={<ViewPost />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/view" element={<ViewPost />} />
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
